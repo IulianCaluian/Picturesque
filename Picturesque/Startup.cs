2c233f04d62b88083f9fbeffe79fca04f89797c8
Caluian Iulian <iulian.caluian@my.fmi.unibuc.ro>
2018-12-08 20:24:46 +0200

    Add project files.

---
+++ Picturesque/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Picturesque.Startup))]
+namespace Picturesque
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
